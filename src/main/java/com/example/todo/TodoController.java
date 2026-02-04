fd59faed6e84cca09b4d0904e17ecca336e66e3c
202510-aw-koshizawa <202510_aw_koshizawa@x-ide.sakura.ne.jp>
2026-02-04 11:21:50 +0900

    ビルドエラー解消（PostMapping の import 不足によるコンパイルエラー）

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoController {
