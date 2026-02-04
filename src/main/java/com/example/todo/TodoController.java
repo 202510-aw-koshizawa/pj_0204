0e81a4a58dc1a17e398eab01fb0d62a3a49fb220
202510-aw-koshizawa <202510_aw_koshizawa@x-ide.sakura.ne.jp>
2026-02-04 10:36:52 +0900

    TodoController を追加

---
+++ src/main/java/com/example/todo/TodoController.java
@@ -0,0 +1,27 @@
+package com.example.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class TodoController {
+
+    // ToDo一覧画面を表示する。
+    @GetMapping("/todos")
+    public String list() {
+        return "todos/list";
+    }
+
+    // ToDo新規作成画面を表示する。
+    @GetMapping("/todos/new")
+    public String createForm() {
+        return "todos/new";
+    }
+
+    // 指定したIDのToDo詳細画面を表示する。
+    @GetMapping("/todos/{id}")
+    public String detail(@PathVariable Long id) {
+        return "todos/detail";
+    }
+}
